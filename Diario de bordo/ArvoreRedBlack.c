74c414c416bfdd26e99e06cd92a019f4c7a3caa4
Christian Porto <christianviking33@gmail.com>
2024-09-20 23:35:05 -0300

    Adiciona funções para realizar as rotações

--- Diario de bordo/ArvoreRedBlack.c
+++ Diario de bordo/ArvoreRedBlack.c
@@ -18,3 +18,39 @@ RBNode* createNode(int data) {
     node->left = node->right = node->parent = NULL;
     return node;
 }
+
+void leftRotate(RBNode **root, RBNode *x) {
+    RBNode *y = x->right;
+    x->right = y->left;
+    if (y->left != NULL)
+        y->left->parent = x;
+    
+    y->parent = x->parent;
+    if (x->parent == NULL)
+        *root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+    
+    y->left = x;
+    x->parent = y;
+}
+
+void rightRotate(RBNode **root, RBNode *y) {
+    RBNode *x = y->left;
+    y->left = x->right;
+    if (x->right != NULL)
+        x->right->parent = y;
+    
+    x->parent = y->parent;
+    if (y->parent == NULL)
+        *root = x;
+    else if (y == y->parent->left)
+        y->parent->left = x;
+    else
+        y->parent->right = x;
+    
+    x->right = y;
+    y->parent = x;
+}
